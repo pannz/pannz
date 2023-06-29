764e7172c238fa42ec3977920e6657e7d37f5217
zhifan.pan <zhifan.pan@grabtaxi.com>
2023-06-29 18:34:45 +0800

    after work save

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import '@unocss/reset/tailwind-compat.css'
+  import 'uno.css'
+</script>
+
+<div class='w-300 m-auto'>
+  <header class='flex justify-between my-4'>
+    <img class="w-10 h-10" src='./favicon.png' alt='zhifan' />
+    <div>navigations</div>
+  </header>
+  <slot />
+</div>
+
+
+<svelte:head>
+  <title>Zhifan</title>
+  <style>
+    body {
+        background: mediumslateblue;
+    }
+  </style>
+</svelte:head>
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,42 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import UnoCSS from 'unocss/vite'
+import presetMini from '@unocss/preset-mini'
+import extractorSvelte from '@unocss/extractor-svelte'
+import presetIcons from '@unocss/preset-icons'
+import Icons from 'unplugin-icons/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [
+		UnoCSS({
+			extractors: [extractorSvelte],
+			presets: [
+				presetMini(),
+				// presetMini({
+				// 	theme: {
+				// 		colors: {
+				// 			grabGreen: '#00b14f',
+				// 		},
+				// 	},
+				// 	rules: [
+				// 		[
+				// 			/^text-(.*)$/,
+				// 			([, c], { theme }) => {
+				// 				if (theme.colors[c]) return { color: theme.colors[c] }
+				// 			},
+				// 		],
+				// 	],
+				// }),
+				presetIcons(),
+			],
+		}),
+		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+			customCollections: {
+				'x-icons': FileSystemIconLoader('./assets/icons', svg => svg.replace(/^<svg /, '<svg fill="currentColor" ')),
+			},
+		}),
+	],
+})
